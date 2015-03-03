d1e0f1fb5c5d990f16b9de6296dd06535875325e
Andre <Andre@Andre-PC>
2015-03-03 00:35:30 -0400

    Trabalho 1 Tap 02/03/2015

---
+++ TAP_1/andreFernande/src/main/java/andreFernande/ClienteServlet.java
@@ -0,0 +1,36 @@
+package andreFernande;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/clienteServlet")
+public class ClienteServlet extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+
+		int id = 0;
+		String nome = req.getParameter("nome");
+		String sexo = req.getParameter("sexo");
+		String email= req.getParameter("email");
+		String cpf = req.getParameter("cpf");
+		String rg = req.getParameter("rg");
+		id++;
+		
+		Cliente cliente = new Cliente(id,nome, sexo, email, cpf, rg);
+
+		req.setAttribute("cliente", cliente);
+		
+		RequestDispatcher dispatcher =  req.getRequestDispatcher("ViewCliente.jsp");
+		dispatcher.forward(req, resp);
+	
+	
+	}
+}
